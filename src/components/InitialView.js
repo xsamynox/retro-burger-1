c32df85f88cf3ed1907e0bf7608b4c2cee25fe5e
Samantha <xsamynox@gmail.com>
2020-09-02 08:33:04 -0400

    Add map for menu items

--- src/components/InitialView.js
+++ src/components/InitialView.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import {
-  Link
-} from "react-router-dom";
+import { Link } from "react-router-dom";
 import logo from '../media/logo.png';
 import bell from '../media/bell-off.png'
-export class ContentHeader extends React.Component{
-  render(){
-    return(
-      <header className=""> 
+export class ContentHeader extends React.Component {
+  render() {
+    return (
+      <header className="">
         <img src={logo} className="" alt="logo" />
         <img src={bell} className="" alt="logo" />
       </header>
-    );  
+    );
   }
-} 
+}
 
 export class View extends React.Component {
   render() {
-    return ( 
+    return (
       <div>
         <ContentHeader />
         <Link to="/mesero"><button className="">Mesero</button></Link>

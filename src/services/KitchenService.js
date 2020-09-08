9e86a1d0849edc98277b806983401e3aae42a5a0
Geraldine Villacura R <geraldinevillacuraruz@gmail.com>
2020-09-08 14:27:58 -0300

    intento mostrar pedido

--- src/services/KitchenService.js
+++ src/services/KitchenService.js
@@ -2,12 +2,10 @@ import firebase from "../firebaseConfig";
 const db = firebase.firestore();
 
 export const getOrder = () => {
-  return db
-    .collection("pedido")
-    .get()
-    .then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} => ${doc.data()}`);
-      });
-    });
+  return db.collection("pedido").get();
+  // .then((querySnapshot) => {
+  //   querySnapshot.forEach((doc) => {
+  //     console.log(`${doc.id} => ${doc.data()}`);
+  //   });
+  // });
 };

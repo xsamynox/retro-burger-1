48af7a247b9f2c22ad81700ee4a41bc1ef3c956b
Geraldine Villacura R <geraldinevillacuraruz@gmail.com>
2020-09-12 17:05:39 -0300

    cronómetro

--- src/components/KitchenView.js
+++ src/components/KitchenView.js
@@ -1,7 +1,8 @@
 import React from "react";
 import firebase from "../firebaseConfig";
 import logo from "../media/logo.png";
-import SecondsCounter from "./Counter";
+import SecondsToString from "./Counter";
+import { idCurrenTime, currentDate } from "../services/MeseroService";
 const db = firebase.firestore();
 
 class ContentHeaderKitchen extends React.Component {
@@ -44,12 +45,6 @@ export class Kitchen extends React.Component {
 
   render() {
     const { kitchenOrders } = this.state;
-    // let counter = 0;
-
-    // setInterval() {
-    //   <SecondsCounter seconds={counter} />;
-    //   counter += 1;
-    // }, 1000);
 
     const kitchentList = kitchenOrders.map((order) => {
       if (order.estadoPedido === "En proceso") {
@@ -59,6 +54,7 @@ export class Kitchen extends React.Component {
             valueButton={order.mesa + " " + order.hora}
             table={order.mesa}
             comments={order.comentario}
+            preDate={order.date + " " + order.hora}
           >
             {order.productos.map((product) => (
               <OrderProducts
@@ -80,35 +76,57 @@ export class Kitchen extends React.Component {
   }
 }
 
-const OrderTable = ({ children, table, valueButton, comments }) => {
-  return (
-    <div className="containerOrderButton">
-      <div className="contailerEachOrderKitchen">
-        <div className="tableNameCounter">
-          <div className="tableNameKitchen">{table}</div>
-          {/* {<div className="counterTime" onLoad={clock()}></div>} */}
-          <SecondsCounter seconds={0} />;
-        </div>
+class OrderTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { seconds: this.getSeconds() };
+  }
 
-        <div className="containerContentProductComments">
-          <div className="containerContentProduct">{children}</div>
-          <div className="containerComment">
-            Comentarios: <br /> {comments}
+  getSeconds() {
+    let thisTime = new Date(currentDate() + " " + idCurrenTime());
+    let prevTime = new Date(this.props.preDate);
+    return (thisTime.getTime() - prevTime.getTime()) / 1000;
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ seconds: this.state.seconds + 1 });
+    }, 1000);
+  }
+
+  render() {
+    return (
+      <div className="containerOrderButton">
+        <div className="contailerEachOrderKitchen">
+          <div className="tableNameCounter">
+            <div className="tableNameKitchen">{this.props.table}</div>
+            {/* {<div className="counterTime" onLoad={clock()}></div>} */}
+            <div className="icon box">
+              <i className="far fa-clock"></i>
+            </div>
+            <SecondsToString seconds={this.state.seconds} />
+          </div>
+
+          <div className="containerContentProductComments">
+            <div className="containerContentProduct">{this.props.children}</div>
+            <div className="containerComment">
+              Comentarios: <br /> {this.props.comments}
+            </div>
           </div>
         </div>
+        <div className="containerBtnReadyKitchen">
+          <button
+            className="btnReadyKitchen"
+            value={this.props.valueButton}
+            onClick={(e) => finishOrder(e)}
+          >
+            Listo
+          </button>
+        </div>
       </div>
-      <div className="containerBtnReadyKitchen">
-        <button
-          className="btnReadyKitchen"
-          value={valueButton}
-          onClick={(e) => finishOrder(e)}
-        >
-          Listo
-        </button>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 const OrderProducts = ({ product }) => {
   return (
